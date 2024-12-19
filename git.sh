b339b8ec9e22571fbb70ce2fa9aacca9524ef03c
wyxdlz54188 <liyuqili_1379zi@qq.com>
2024-12-19 12:20:36 +0000

    updata

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 #!/var/jb/usr/bin/bash
 echo "bilibili@玩科技的李子"
 echo "token密钥"
-echo "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"
+echo "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"
 echo " "
 echo "__________________________________"
 echo "__________________________________"
